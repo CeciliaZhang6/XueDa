47f31bbbf627f9bcc4a5456abc1405801f3f47a0
Cecilia Zhang <107581576+CeciliaZhang6@users.noreply.github.com>
2024-07-03 15:36:39 -0700

    Add files via upload

---
+++ main/roomAPI.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once('dbh.php');
+
+$sql = "SELECT * FROM rooms";
+
+$api = [];
+
+$res = $conn->query($sql);
+
+if ($res->num_rows() > 0){
+    while($row = $res->fetch_assoc()){
+        $api[] = $row;
+    }
+}
+
+$jsonAPI = json_encode($api);
+echo $jsonAPI;
+
+?>
