1d3dea12b9734b7420199472bcfe72afcb66513d
CeciliaZhang6 <ceciliazhang0923@gmail.com>
2024-07-10 16:35:40 -0700

    added cur_user_post()

--- main/functions.php
+++ main/functions.php
@@ -63,10 +63,57 @@ function create_base_dir($conn, $uid){
     $res = mkdir($path);
 
     if ($res === FALSE){
-        die("mkdir failed");
+        die("mkdir failed - rooms");
+    }
+
+    $path = "/" . $base_dir . "/" . $uid . "/" . $sub_dir_2;
+
+    $res = mkdir($path);
+
+    if ($res === FALSE){
+        die("mkdir failed - pfp");
     }
 
 
+}
+
+// sql is the query to get all data, used to create api
+// email is the email of target user
+function cur_user_post($conn, $sql, $email){
+    $api = [];
+    $res = $conn->query($sql);
+
+    if ($res->num_rows > 0){
+        while($row = $res->fetch_assoc()){
+            $api[] = $row;
+        }
+    }
+
+    $api_json = json_encode($api);
+    echo $api_json;
+
+    $dir = "/home/uccaciyo/public_html/csp1/users/" . $email;
+
+    // create dir if not exist
+    if (!is_dir($dir)){
+        mkdir_($dir, 0777, true);
+    }
+
+    $file_path = $dir . "/cur_user_post.json";
+    echo $file_path;
+
+    // check write permission
+    if (!is_writable($directory)){
+        die("Cannot write to dir:" . $dir);
+    }
+
+    $res = file_put_contents($file_path, $api_json);
+    if ($res === false){
+        $error = error_get_last();
+        die("Cannot write. Error:" . $error['message']);
+    } else {
+        echo "File written done";
+    }
 }
 
 ?>
