0857ae7b5b06fc064a0b7a7b7240597e51287af5
CeciliaZhang6 <ceciliazhang0923@gmail.com>
2024-08-12 16:12:00 -0700

    fixing~~~

--- main/delete_post.php
+++ main/delete_post.php
@@ -3,7 +3,7 @@
 session_start();
 include_once("functions.php");
 
-$room_id = $_POST['id'];
+$room_id = $_POST['room_id'];
 $email = $_SESSION['curr_user'];
 
 delete_post($conn, $room_id, $email);
