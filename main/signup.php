14a8d6f93d59dfe0684b26f926c3236f2ea57238
ucb1101 <codingpro1101@gmail.com>
2024-08-19 15:25:47 -0700

    sssss

--- main/signup.php
+++ main/signup.php
@@ -4,32 +4,55 @@
 session_start();
 include_once("functions.php");
 
-// Assuming $conn is properly initialized in functions.php
+
+// assuming $conn is properly initialized in functions.php
 $email = $_POST['email'];
 $password = $_POST['password'];
-$date = date('Y-m-d H:i:s');
+$date = date('Y-m-d');
+
+
+
 
 if (isset($email) && isset($password)) {
+
     // validate params
     user_res_auth($conn, $email);
-    
-    // Prevent SQL injection by using prepared statements
-    $stmt = $conn->prepare("INSERT INTO users (email, pass_word, creation_date) VALUES (?, ?, ?)");
-    
-    // Hash the password before storing it
-    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+   
+    // prevent SQL injection by using prepared statements
+    $stmt = $conn->prepare("INSERT INTO users (email, pass_word, pass__word, user_name, creation_date) VALUES (?, ?, ?, ?, ?)");
 
-    $stmt->bind_param("sss", $email, $hashed_password, $date);
+   
+
+    // generate a default username
+    $first_letter = chr(rand(65, 90)); // generates a random uppercase letter (A-Z)
+    
+   
+    
+    $default_username = generateRandomString();
+    echo $default_username;
+    
+    // embed password
+    $e_password = $first_letter . $password . $default_username;
+
+
+    // hash the password before storing it
+    $h_password = password_hash($password, PASSWORD_BCRYPT);
+
+
+    $stmt->bind_param("sssss", $email, $e_password, $h_password, $default_username, $date);
+
 
     if ($stmt->execute()) {
         echo "User data inserted successfully\n";
         echo "Email: $email \n";
 
+
         create_base_dir($conn, $email);
+
 
         $_SESSION["login_status"] = TRUE;
         $_SESSION["curr_user"] = $email;
-        
+       
         $stmt->close();
         $conn->close();
         header("Location: index.php");
@@ -37,10 +60,13 @@ if (isset($email) && isset($password)) {
     } else {
         die("Error inserting into user table: " . $stmt->error);
     }
-  
+ 
 } else {
     die("ERROR: empty email or password");
 }
 
+
 $conn->close();
 ?>
+
+
