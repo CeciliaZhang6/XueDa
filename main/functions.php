5344cf6f0304bd5662bfcfe2e6c7ce1e8bf064bd
ucb1101 <codingpro1101@gmail.com>
2024-08-19 15:26:35 -0700

    updated randomGen

--- main/functions.php
+++ main/functions.php
@@ -255,4 +255,15 @@ function delete_post($conn, $id, $email){
     $conn->close();
 }
 
+function generateRandomString($length = 6) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+
 ?>
